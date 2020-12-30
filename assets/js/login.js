bafe6a628a455af8021c229eff8bb5b65bb8d2a6
sx <240718221@qq.com>
2020-12-30 19:23:28 +0800

    index布局

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,4 +9,39 @@ $(function () {
     $(".login-box").show()
     $(".reg-box").hide()
   })
+  //自定义表单验证
+  const form = layui.form
+  form.verify = {
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位,切不能出现空格"],
+    repwd: function (value) {
+      var pwd = $(".reg-box [name=password]").val()
+      if (pwd !== value) {
+        return "两次密码不一致"
+      }
+    },
+  }
+  let layer = layui.layer
+  //注册功能
+  $("#form_reg").submit(function (e) {
+    e.preventDefault()
+    let data = {
+      username: $("#form_reg [name=username]").val(),
+      password: $("#form_reg [name=password]").val(),
+    }
+    $.post("http://ajax.frontend.itheima.net/api/reguser", data, (res) => {
+      if (res.status !== 0) return alert(res.message)
+      layer.msg("注册成功")
+      $("#link_login").click()
+    })
+  })
+
+  $("#form_login").on("submit", function (e) {
+    e.preventDefault()
+    let data = $(this).serialize()
+    $.post("http://ajax.frontend.itheima.net/api/login", data, (res) => {
+      layer.msg("登录成功")
+      localStorage.setItem("token", res.token)
+      setTimeout((location.href = "/index.html"), 1000)
+    })
+  })
 })
