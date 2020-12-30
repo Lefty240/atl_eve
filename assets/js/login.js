c0be46382069f9f6125da33e627a7ee90ad7612c
sx <240718221@qq.com>
2020-12-30 14:39:55 +0800

    登录注册页面

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+$(function () {
+  $("#link_reg").on("click", function () {
+    $(".login-box").hide()
+    $(".reg-box").show()
+  })
+
+  // 点击“去登录”的链接
+  $("#link_login").on("click", function () {
+    $(".login-box").show()
+    $(".reg-box").hide()
+  })
+})
